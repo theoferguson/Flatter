684f5c81ff63f1c2ec3a090abbc13f39aa636ab2
Theo Ferguson <ferguson.theo@gmail.com>
2021-10-25 17:53:41 -0400

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:username])
-        if user
+        if user&.authenticate(params[:password])
             session[:user_id] = user.id
-            render json: user
+            render json: user, status: :created
         else
             render json: { error: 'Not a current username' }, status: :unauthorized
         end
